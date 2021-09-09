cbb4c3b6e8f7f55bac14c8c9ef4b043516edd04f
abhishek <abhishek.kumar@biofourmis.com>
2021-09-10 02:45:06 +0530

    cleared files

--- address/forms.py
+++ address/forms.py
@@ -1,14 +1,2 @@
-# from .models import User as CustomUser
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
-# class SignUpForm(UserCreationForm):
-#     class Meta:
-#         model = CustomUser
-#         fields = ["email","full_name","phone_number"]
-#         exclude = ["is_active","staff", "admin","last_login"]
-
-# class LoginForm(forms.Form):
-#     email = forms.EmailField(label="Email")
-#     #phone_number = forms.CharField()
-#     password = forms.CharField(widget=forms.PasswordInput)
