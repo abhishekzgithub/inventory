785e233280dc845bc4c8ac9cd899db21b7f2186f
abhishek <abhishek.kumar@biofourmis.com>
2022-01-26 02:04:09 +0530

    added graphs

--- analytics/urls.py
+++ analytics/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r"^piechart/$", analytics_views.display_piechart, name="piechart"),
     url(r"^linechart/$", analytics_views.display_line_chart, name="linechart"),
     url(r"^barchart/$", analytics_views.display_bar_chart, name="display_bar_chart"),
+    url(r"^order/(?P<status>[\w]+)/$", analytics_views.display_order_status, name="display_order_status"),
 
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from order.models import Order
+from product.models import Product
+from expense.models import Expense
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
+@login_required(login_url=settings.LOGIN_URL)
 def display_piechart(request, context={}):
     labels = ["January","February","March","April","May"]
     data = [65, 59, 80, 81, 56]
@@ -17,6 +23,7 @@ def display_piechart(request, context={}):
     context["data"]=data
     return render(request, "analytics/piechart.html", context)
 
+@login_required(login_url=settings.LOGIN_URL)
 def display_line_chart(request, context={}):
     labels = ["January","February","March","April","May"]
     data = [65, 59, 80, 81, 56, 55, 40]
@@ -34,7 +41,7 @@ def display_line_chart(request, context={}):
     context["data"]=data
     return render(request, "analytics/linechart.html", context)
 
-
+@login_required(login_url=settings.LOGIN_URL)
 def display_bar_chart(request, context={}):
     labels = ["January","February","March","April","May"]
     data = [65, 59, 80, 81, 56, 55, 40]
@@ -51,3 +58,24 @@ def display_bar_chart(request, context={}):
     context["labels"]=labels
     context["data"]=data
     return render(request, "analytics/barchart.html", context)
+
+@login_required(login_url=settings.LOGIN_URL)
+def display_order_status(request, status="created"):
+    order_count = Order.objects.filter(status=status,ordered=True).count()
+    return order_count
+
+@login_required(login_url=settings.LOGIN_URL)
+def display_all_orders(request):
+    all_order = Order.objects.filter(ordered=True).all()
+    return all_order
+
+
+@login_required(login_url=settings.LOGIN_URL)
+def display_all_expense(request):
+    all_expense = Expense.objects.filter(user=request.user).all()
+    return all_expense
+
+@login_required(login_url=settings.LOGIN_URL)
+def display_total_expense(request):
+    total_expense = Expense.objects.filter(user=request.user).count()
+    return total_expense
